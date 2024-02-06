bf5af73e1da3fe576ff0d20a3421d3a908aed4af
michele-pupo <mikypupo@hotmail.it>
2024-02-06 14:55:41 +0100

    exercise 1 structure created

--- Pari e dispari/js/script.js
+++ Pari e dispari/js/script.js
@@ -1,37 +1,79 @@
-// Snack 1:
-// - Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. 
-// - Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50
+// Pari e Dispari
+// - L’utente sceglie pari o dispari.
+// - Inserisce un numero da 1 a 5.
+// - Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// - Sommiamo i due numeri
+// - Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// - Se l'utente ha indovinato l'esito (pari o dispari) della somma ha vinto, altrimenti ha perso.
 
-// inizializziamo un array vuoto
-let userNumbersList = [];
 
-// inizializziamo una costante somma uguale a 0
-let sum = 0;
+// creiamo una funzione per far scegliere all'utente, tramite prompt, pari o dispari
+function scegliPariODispari() {
+    let choice = prompt("Cosa scegli? PARI o DISPARI... ");
 
-//  creiamo un ciclo while che sommi i numeri contenuti nell'array finche la somma di essi sia minore di 50
-while (sum < 50) {
+    // controlliamo che l'utente abbia scritto pari o dispari
+    while (choice !== 'pari' && choice !== 'dispari') {
 
-    let userNumber = Number(prompt("Inserisci un numero"));
-    // console.log(userNumber);
+        // forziamo la scrittura della parola indicata
+        choice = prompt("Puoi scegliere solo 'PARI' o 'DISPARI', non sono consentite altre parole o numeri");
+    }
+    return choice;
+}
 
-    // controllo che il numero inserito non sia nullo e che sia un numero
-    if(userNumber == null || isNaN(userNumber)) {
+// creiamo una funzione per far scegliere all'utente, tramite prompt, un numero da 1 a 5
+function inserisciNumero() {
+    let number = parseInt(prompt("Scegli un numero da 1 a 5... "));
 
-    alert("Non hai inserito un numero! Questo dato non verrà sommato");
+    // controlliamo che l'utente abbia scritto un numero da 1 a 5
+    while (number < 1 || number > 5 || isNaN(number)) {
 
+        // forziamo la scrittura del numero indicato
+        number = parseInt(prompt("Puoi scegliere solo un numero da '1' a '5', non sono consentite altre parole o numeri"));
+    }
+    return number;
+}
+
+// creiamo una funzione per far generare al computer un numero casuale da 1 a 5
+function generaNumero() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+// creiamo una funzione per far sommare i due numeri
+function sommaPariODispari(num1, num2) {
+    let sum = num1 + num2;
+
+    // se la somma dei numeri è divisibile per 2, allora il numero sarà pari
+    if (sum % 2 === 0) {
+        return 'pari';
+
+      // se la somma dei numeri non è divisibile per 2, allora il numero sarà dipari  
     } else {
-        
-    // inserisco i numeri nell'array
-    userNumbersList.push(userNumber);
-
-    // sommo i numeri inseriti
-    sum += userNumber;
-
-    // stampo a schermo la somma
-    // console.log(sum);
-    document.querySelector("#sum-numbers").innerHTML = `${sum}`;
+        return 'dispari';
     }
 }
 
+// creiamo una funzione per effettuare il gioco
+function gioca() {
+
+    // richiamo la funzione della scelta di pari o dispari
+    let userChoice = scegliPariODispari();
+
+    // richiamo la funzione dell'inserimento del numero utente
+    let userNumber = inserisciNumero();
+
+    // richiamo la funzione del numero casuale genarato dal computer
+    let computerNumber = generaNumero();
+    alert("Il computer ha scelto il numero: " + computerNumber);
+    let result = sommaPariODispari(userNumber, computerNumber);
+    alert("La somma dei due numeri è: " + (userNumber + computerNumber));
+    if (result === userChoice) {
+        alert(`Poichè hai scelto "${userChoice}" Hai vinto!`);
+    } else {
+        alert(`Poichè hai scelto "${userChoice}" Hai perso!`);
+    }
+}
+
+// richiamo la funzione per eseguire il gioco
+gioca();
 
 
