7bc02b7464d507b674b4bd9638588f46a7d7f617
michele-pupo <mikypupo@hotmail.it>
2024-02-06 15:34:23 +0100

    exercise 2 structure created

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,37 +1,47 @@
-// Snack 2
-// - Genera un numero casuale tra 1 e 100 e poi chiedi all'utente di indovinare il numero. 
-// - Il programma poi deve far comparire un messaggio "il numero è troppo alto" 
-//   oppure "il numero è troppo basso" nel momento in cui il numero detto dall'utente 
-//   non è perfettamente uguale al numero estratto in modo casuale.
-// - Quando l'utente trova il numero, fateglielo sapere!
+// Palidroma
+// - Chiedere all’utente di inserire una parola.
+// - Creare una funzione per capire se la parola inserita è palindroma
+//   (ad esempio, questa funzione potrebbe funzionare così: passandole la parola come parametro, 
+//    farà i suoi calcoli per capire se è palindroma o meno e ci restituirà true o false 
+//    in base all'esito)
 
-// Genera un numero casuale tra 1 e 100
-const randomNumber = Math.floor((Math.random() * 100) + 1);
-console.log(randomNumber);
+// creiamo la funzione palindroma
+function isPalindroma(word) {
 
-let attempts = 0;
-let win = false;
+  // confronto che i caratteri inseriti siano gli stessi dall'inizio alla fine e viceversa, (proseguendo a ritroso)
+  // dividiamo la parola in 2 e controlliamo che la prima parte sia uguale all'inverso della seconda
+  for (let i = 0; i < Math.floor(word.length / 2); i++) {
 
-do {
+  
+      if (word[i] !== word[word.length - 1 - i]) {
 
-    // creo un ciclo do-while per consentire all'utente di indovinare il numero
-    let attemptNumber = parseInt(prompt("Indovina il numero (da 1 a 100):"));
-
-    
-      // Controlla se il numero è corretto
-      if (attemptNumber === randomNumber) {
-        console.log("Complimenti! Hai indovinato il numero:", randomNumber);
-
-        win = true;
-    
-      } else if (attemptNumber < randomNumber) {
-        console.log("Il numero è troppo basso.");
-
-      } else {
-        console.log("Il numero è troppo alto.");
-
+          // se i caratteri inseriti non corrispondono allora la parola inserita non è palindroma
+          return false;
       }
-    } while (!win);
+  }
+  
+  // se i caratteri inseriti corrispondono allora la parola inserita è palindroma
+  return true;
+}
+
+// chiediamo all'utente di inserire la parola o un numero tramite prompt
+let userWord = prompt("Inserisci una parola o un numero");
+
+// creo una costante per stampare in pagina il risultato
+const resultElement = document.querySelector("#esito");
+
+if (isPalindroma(userWord)) {
+
+  // i caratteri corrispondono, la parola è palindroma
+  // console.log(`La parola o il numero "${userWord}" è palindroma.`);
+  resultElement.innerText = `La parola o il numero "${userWord}" è palindroma.`;
+
+} else {
+
+  // i caratteri non corrispondono, la parola non è palindroma
+  // console.log(`La parola o il numero "${userWord}" non è palindroma.`);
+  resultElement.innerText = `La parola o il numero "${userWord}" non è palindroma.`;
+}
 
  
 
